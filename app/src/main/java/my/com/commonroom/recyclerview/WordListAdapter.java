1ddb57ece0049922a19632557a066d2c3f513686
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2018-09-11 10:59:31 +0800

    first commit

---
+++ app/src/main/java/my/com/commonroom/recyclerview/WordListAdapter.java
@@ -0,0 +1,63 @@
+package my.com.commonroom.recyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.custom_row, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        // Add the data to the view holder.
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount()  {
+        return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        /**
+         * Creates a new custom view holder to hold the view to display in the RecyclerView.
+         *
+         * @param itemView The view in which to display the data.
+         * @param adapter The adapter that manages the the data and views for the RecyclerView.
+         */
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = (TextView) itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // All we do here is prepend "Clicked! " to the text in the view, to verify that
+            // the correct item was clicked. The underlying data does not change.
+            wordItemView.setText ("Clicked! "+ wordItemView.getText());
+        }
+    }
+}
